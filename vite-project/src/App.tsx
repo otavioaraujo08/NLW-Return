38d6ecb747fbbb72a651f279dca375c141aaeb7c
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-05-03 20:44:39 -0300

    Reformulando o main

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,19 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from "./components/widget";
 
-function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? 'Default'}</button>
+export function App() {
+  return <Widget />
 }
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="OLá mundo"/>
-      <Button text="OLá país"/>
-      <Button text="OLá cidade"/>
-    </div>
-  )
-}
-
-export default App
